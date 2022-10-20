632a5083392174c8638edda42d592fe5b42e14d4
siki2-uiuc <siki2@illinois.edu>
2022-10-20 17:27:30 +0000

    Got post bulletin working dynamically

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,9 @@ class PostsController < ApplicationController
 
   def index
 
+    all_posts = Post.all
+
+    @list_of_posts = all_posts.order({:created_at => :asc})
     render({ :template => "post_templates/index.html.erb"})
   end
 end
