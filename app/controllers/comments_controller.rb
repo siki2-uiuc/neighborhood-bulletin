059a864f125c336b4668d8cc0e8b44dc1648daf3
siki2-uiuc <siki2@illinois.edu>
2022-10-21 19:51:08 +0000

    Create comment functionality working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,20 @@
 class CommentsController < ApplicationController
   def add_comment
+    post_id = params.fetch("query_post_id")
 
-    redirect_to("/posts/")
+    commenting_author_name = params.fetch("query_author_name")
+    comment_body = params.fetch("query_comment")
+
+    commenting_author = User.where({ :username => commenting_author_name}).first
+    commenting_author_id = commenting_author.id
+
+    new_comment = Comment.new
+    new_comment.post_id = post_id
+    new_comment.author_id = commenting_author_id
+    new_comment.body = comment_body
+
+    new_comment.save
+
+    redirect_to("/posts/#{new_comment.post_id}")
   end
 end
